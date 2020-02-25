27bc72acd00b18b356090aa0986527a25d045f70
Bryant Omoregie <unclebryant@gmail.com>
2020-02-25 05:20:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,5 +43,13 @@ def reduce_to_total(source_array, starting_point = 0 )
 end 
 
 def reduce_to_all_true(source_array)
-source_array.each do |number|
+if source_array.each == truthy 
+return true 
+end
+end 
+
+
+
+
+
 
